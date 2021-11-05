62b1b2824cb812fb4f6d46696d2eff058b68f2f6
drumm2k <drumm2k@gmail.com>
2021-11-06 01:01:29 +0300

    Add StatusBar back

--- App.tsx
+++ App.tsx
@@ -9,48 +9,35 @@ const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <>
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName
 
-              if (route.name === 'Home') {
-                iconName = focused ? 'home' : 'home-outline'
-              } else if (route.name === 'Trade') {
-                iconName = focused ? 'file-tray' : 'file-tray-outline'
-              }
+            if (route.name === 'Home') {
+              iconName = focused ? 'home' : 'home-outline'
+            } else if (route.name === 'Trade') {
+              iconName = focused ? 'file-tray' : 'file-tray-outline'
+            }
 
-              // @ts-expect-error
-              return <Ionicons name={iconName} size={size} color={color} />
-            },
-            tabBarActiveTintColor: 'tomato',
-            tabBarInactiveTintColor: 'gray'
-          })}
-        >
-          <Tab.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{
-              title: 'Home'
-            }}
-          />
-          <Tab.Screen name="Trade" component={TradeScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
-      {/* <SafeAreaView style={styles.container}>
-        <HomeScreen />
-        <TradeScreen />
-      </SafeAreaView>
-      <ExpoStatusBar style="auto" /> */}
-    </>
+            // @ts-expect-error
+            return <Ionicons name={iconName} size={size} color={color} />
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray'
+        })}
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: 'Home'
+          }}
+        />
+        <Tab.Screen name="Trade" component={TradeScreen} />
+      </Tab.Navigator>
+      <ExpoStatusBar style="auto" />
+    </NavigationContainer>
   )
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-//   },
-// });
