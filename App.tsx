ed64f48b1872ebb9935a8fd50934386b22f4ddfd
drumm2k <drumm2k@gmail.com>
2021-11-05 19:04:03 +0300

    Testing pokemons cards list render

--- App.tsx
+++ App.tsx
@@ -1,21 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as StatusBarExpo } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  View,
+  Text,
+  FlatList,
+} from "react-native";
+import { Card } from "./components";
+
+const data: any[] = [];
+
+for (let i = 1; i < 100; i += 4) {
+  data.push([
+    {
+      id: i,
+      name: `name ${i}`,
+      gen: "1 gen",
+      types: ["water"],
+      img: `https://pokego.ru/img/pokemon/${i}.png`,
+    },
+    {
+      id: i + 1,
+      name: `name ${i + 1}`,
+      gen: "1 gen",
+      types: ["water"],
+      img: `https://pokego.ru/img/pokemon/${i + 1}.png`,
+    },
+    {
+      id: i + 2,
+      name: `name ${i + 2}`,
+      gen: "1 gen",
+      types: ["water"],
+      img: `https://pokego.ru/img/pokemon/${i + 2}.png`,
+    },
+    {
+      id: i + 3,
+      name: `name ${i + 3}`,
+      gen: "1 gen",
+      types: ["water"],
+      img: `https://pokego.ru/img/pokemon/${i + 3}.png`,
+    },
+  ]);
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Filters</Text>
+      <FlatList
+        data={data}
+        keyExtractor={(item, index) => item[0].id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.cardWrapper} key={item[0].id}>
+            {item.map((pokemon: any) => (
+              <Card
+                key={pokemon.id}
+                pokedex={pokemon.id}
+                gen={pokemon.gen}
+                types={pokemon.types}
+                name={pokemon.name}
+                img={pokemon.img}
+                onPress={() => alert(pokemon.id)}
+              />
+            ))}
+          </View>
+        )}
+      />
+      <StatusBarExpo style="auto" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  cardWrapper: {
+    paddingLeft: 10,
+    paddingRight: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    flexWrap: "wrap",
   },
 });
---
+++ components/Card/index.tsx
@@ -0,0 +1,88 @@
+import React, { FC } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
+export interface PokeCardType {
+  pokedex: number;
+  name: string;
+  types: string[];
+  gen?: string;
+  img: string;
+  onPress: () => void;
+}
+
+const PokeCard: FC<PokeCardType> = ({
+  pokedex,
+  name,
+  types,
+  gen,
+  img,
+  onPress,
+}) => {
+  return (
+    <TouchableOpacity onPress={onPress} activeOpacity={0.7}>
+      <View style={styles.card}>
+        <LinearGradient colors={["#4c669f", "#3b5998", "#192f6a"]}>
+          <ImageBackground
+            style={styles.image}
+            source={{ uri: img }}
+            resizeMode="cover"
+          >
+            {/* <CardImg imgUrl={img && img}> */}
+            {pokedex && <Text style={styles.topText}>#{pokedex}</Text>}
+            {gen && <Text style={styles.topText}>{gen}</Text>}
+            {/* </CardImg> */}
+          </ImageBackground>
+        </LinearGradient>
+
+        <Text style={styles.name}>{name}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    maxWidth: 84,
+    overflow: "hidden",
+    fontSize: 12,
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: "solid 1px hsla(0, 0%, 0%, 0.12)",
+    marginTop: 3,
+    marginBottom: 3,
+    marginLeft: 1,
+    marginRight: 1,
+  },
+  topText: {
+    fontSize: 12,
+    color: "#fff",
+  },
+  image: {
+    minWidth: 82,
+    minHeight: 82,
+    overflow: "hidden",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingLeft: 5,
+    paddingRight: 5,
+    color: "#fff",
+    // letterSpacing: "1px",
+  },
+  name: {
+    fontSize: 14,
+    textAlign: "center",
+    textTransform: "capitalize",
+    paddingLeft: 5,
+    paddingRight: 5,
+  },
+});
+
+export default PokeCard;
