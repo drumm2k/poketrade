24e815ec7895780d35a5349d4b3f040c2d744fee
drumm2k <drumm2k@gmail.com>
2021-11-07 00:07:52 +0300

    Fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,18 +8,17 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier'
+    'plugin:prettier/recommended'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
     ecmaFeatures: {
       jsx: true
     },
     ecmaVersion: 13,
     sourceType: 'module'
   },
-  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
   rules: {
     indent: ['error', 2, { SwitchCase: 1 }],
     quotes: ['error', 'single', { avoidEscape: true }],
